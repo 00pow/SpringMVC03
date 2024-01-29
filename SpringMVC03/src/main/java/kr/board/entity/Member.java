34d588416b7bb1449d9a2d2823780c1bcc712c7d
00pow <kimseo131612@gmail.com>
2024-01-29 14:51:36 +0900

    entity create

---
+++ SpringMVC03/src/main/java/kr/board/entity/Member.java
@@ -0,0 +1,12 @@
+package kr.board.entity;
+
+public class Member {
+	  private int memIdx; 
+	  private String memID;  
+	  private String memPassword;
+	  private String memName;
+	  private int memAge; // <-null, 0
+	  private String memGender;
+	  private String memEmail;
+	  private String memProfile; //사진정보
+}
